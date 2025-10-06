7fbe1e8c4a594768cf703bc57c05393224f060c5
Fayis <fayizpachu217@gmail.com>
2025-10-06 16:40:20 +0530

    hosting url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://canopus-backend.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
