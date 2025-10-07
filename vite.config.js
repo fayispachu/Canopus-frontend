82d4494ceb8c2bc4097dd4ecc6c4ddf02209559f
Fayis <fayizpachu217@gmail.com>
2025-10-07 13:14:52 +0530

    added vercel.json file and edited vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {},
+  },
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
