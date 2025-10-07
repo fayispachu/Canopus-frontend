7781db756c94fe83eed8dcf3ee632a77b1c430c7
Fayis <fayizpachu217@gmail.com>
2025-10-07 20:48:23 +0530

    added vercel.json file and edited vite config file

--- src/AppWrapper.jsx
+++ src/AppWrapper.jsx
@@ -1,46 +1,41 @@
 import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import OfferingList from "./pages/OfferingList";
-import Header from "./components/Header";
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
 import Dashboard from "./pages/Dashboard";
-import FloatingBooking from "./components/FloatingBooking";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function AppWrapper() {
   // No useLocation needed for HashRouter, optional
   return (
-    <HashRouter>
-      {/* Conditionally render Header/FloatingBooking if needed */}
-      <Routes>
-        {/* Public */}
-        <Route path="/" element={<Home />} />
-        <Route path="/offerings" element={<OfferingList />} />
-        <Route path="/login" element={<Login />} />
+    <Routes>
+      {/* Public */}
+      <Route path="/" element={<Home />} />
+      <Route path="/offerings" element={<OfferingList />} />
+      <Route path="/login" element={<Login />} />
 
-        {/* Protected */}
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <UserProfile />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute adminOnly>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+      {/* Protected */}
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <UserProfile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute adminOnly>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
 
-        {/* Catch-all */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </HashRouter>
+      {/* Catch-all */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
